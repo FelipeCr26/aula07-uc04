084a8541922a1c79c355177a669ef568c419737c
Felipe Crecencio <crecencio.9978@aluno.pr.senac.br>
2024-11-26 20:56:28 -0300

    origin main

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// Crie um programa que valida se uma pessoa pode entrar em uma festa
+// As regras para a pessoa entrar são:
+// A pessoa precisa ter 18 anos ou mais
+// Ou caso a pessoa tenha menos de 18 anos, deve estar acompanhada de um responsável
+
+//ENTRADA
+const idadePessoa = 18
+const estaComResponsavel = false
+
+
+//PROCESSAMENTO
+
+//if - se
+if (idadePessoa > 18 || idadePessoa == 18) {
+    //SAIDA
+    console.log("Entrada Liberada")
+} else {
+    if (estaComResponsavel === true)
+}
+
+
